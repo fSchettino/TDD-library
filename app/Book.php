06f27edb3b21addf035d8502b6e45acb9e6bfa4f
Fabio Schettino <fabio.schettino.01@gmail.com>
2020-02-15 22:23:31 +0100

    adds path method for book model

--- app/Book.php
+++ app/Book.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     protected $table = 'book';
     protected $fillable = ['title', 'author'];
+
+    public function path()
+    {
+        return '/books/' . $this->id . '-' . Str::slug($this->title);
+    }
 }
