f76cf4dafa4133954a324874a6b943d42318c089
Fabio Schettino <fabio.schettino.01@gmail.com>
2020-02-17 22:09:57 +0100

    guarded attribute added

--- app/Author.php
+++ app/Author.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     protected $table = 'author';
-    protected $fillable = ['name', 'dob'];
+    //protected $fillable = ['name', 'dob'];
+    protected $guarded = [];
     protected $dates = ['dob'];
 
     // Mutator
-    public function setDobAttribute($dob) // Follow laravel namng convention set+Column-name+Attribute
+    public function setDobAttribute($dob) // Follow laravel naming convention set+Column-name+Attribute
     {
         // Gets dob model attribute from model attributes array and parse it to Carbon object
         $this->attributes['dob'] = Carbon::parse($dob);
